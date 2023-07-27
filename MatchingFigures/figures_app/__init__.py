7ccc1cf874055f1cfe6f49a87dc5d82b7a30d5dc
KlogeBukser <keranchen@protonmail.com>
2023-07-28 00:12:47 +0800

    update: included test figures

--- MatchingFigures/figures_app/__init__.py
+++ MatchingFigures/figures_app/__init__.py
@@ -9,7 +9,7 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'figures'
     PLAYERS_PER_GROUP = 2 # people who are in the same group, None if all are in the same group
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 2
     PAYMENT_PER_CORRECT = 1
     NUM_FIGURES = 6
     CORRECT_RESULTS = (4,2,5,1,3,6) # TODO: need to be randomised
@@ -21,6 +21,7 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
+    # The correct results should be placed here
     pass
 
 
@@ -28,33 +29,52 @@ class Player(BasePlayer):
     '''All variables in the Player is for the current round.'''
     # payoff and round_number are defined in the background, don't redefine it.  
 
+
     result0 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 1 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 1 on YOUR SCREEN"
         )
     result1 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 2 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 2 on YOUR SCREEN"
         )
     result2 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 3 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 3 on YOUR SCREEN"
         )
     result3 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 4 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 4 on YOUR SCREEN"
         )
     result4 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 5 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 5 on YOUR SCREEN"
         )
     result5 = models.IntegerField(
-        label=f"Please enter the label of the figure on your partner's screen that matches the 6 figure on YOUR SCREEN"
+        label=f"Please enter THE LABEL of the figure on YOUR PARTNER'S SCREEN that matches figure 6 on YOUR SCREEN"
         )
     
     def get_results(self):
         return np.array([self.result0,self.result1, self.result2, self.result3, self.result4, self.result5])
 
+    
 # PAGES
 class MyPage(Page):
     form_model = 'player'
-    form_fields = ['result0', 'result1', 'result2', 'result3', 'result4', 'result5']
 
+    def get_form_fields(player: Player):
+        if player.id_in_group == 1:
+            # The first player's ID is 1
+            return ['result0', 'result1', 'result2', 'result3', 'result4', 'result5']
+        else:
+            return []
+
+    def vars_for_template(self):
+        # For example, let's say you have six figures in a specific order:
+        
+        if self.id_in_group == 1:
+            ordered_figures = ['global/4.png', 'global/2.png', 'global/5.png', 'global/1.png', 'global/3.png', 'global/6.png']
+        else:
+            ordered_figures = ['global/1.png', 'global/2.png', 'global/3.png', 'global/4.png', 'global/5.png', 'global/6.png']
+            
+        return {
+            'ordered_figures': ordered_figures
+        }
 
 class ResultsWaitPage(WaitPage):
     @staticmethod
@@ -66,7 +86,8 @@ class ResultsWaitPage(WaitPage):
 
         results = main_player.get_results()
         main_player.payoff = C.NUM_FIGURES - np.count_nonzero(results-np.array(C.CORRECT_RESULTS))
-
+        for player in group.get_players():
+            player.payoff = main_player.payoff
     
 
 class Results(Page):
