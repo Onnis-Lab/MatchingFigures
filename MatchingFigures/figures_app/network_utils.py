0d2cf1fdfbe476ce4d7fbecb7e59bacee313d4c9
KlogeBukser <keranchen@protonmail.com>
2023-08-15 13:49:35 +0800

    update: added .csv writing, visualisation of diffusion, round counts and scheduler

--- MatchingFigures/figures_app/network_utils.py
+++ MatchingFigures/figures_app/network_utils.py
@@ -3,48 +3,20 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import datetime
+import random
+random.seed(4242)
 
 '''
 Contains functions for creating networks and (potentially) matchmaking algorithms. 
 '''
 
-#TODO: Watts-Strogatz 
-def regularise_network(G,k):
-    
-    #TODO: remove this function
-
-    ''' 
-        Remove extra neighbours of the given network until it becomes a regular network with the given number of neighbors.
-        Inputs:
-        G: graph, 
-            the graph to be regularised with MORE edges than desired;
-        k: int,
-            the number of neighbours for each node.
-        
-    '''
-    for node in G.nodes:
-        while len(list(G.neighbors(node))) > k:
-            # Randomly select a neighbor to disconnect
-            neighbor = np.random.choice(list(G.neighbors(node)))
-            G.remove_edge(node, neighbor)
-
-        # non_neighbors = list(nx.non_neighbors(G, node))
-        # while len(list(G.neighbors(node))) < k:
-        #     # Randomly select a non-neighbor node to connect
-            
-        #     if non_neighbors:  # If there are non-neighbors available
-        #         new_neighbor = np.random.choice(non_neighbors)
-        #         if get_distance(G, node, new_neighbor) < max_dist:
-        #             G.add_edge(node, new_neighbor)
-        #         non_neighbors.remove(new_neighbor)
-        #     else:
-        #         break
-
+#TODO: save the network
 
 # Draw the graph
-def draw(G, pos=None, with_labels=True):
+def draw(G, pos=None, with_labels=True, node_color='skyblue'):
     if pos:
-        nx.draw(G, pos, with_labels=with_labels, node_size=100, node_color='skyblue')
+        nx.draw(G, pos, with_labels=with_labels, node_size=100, node_color=node_color)
     else:
         nx.draw(G, with_labels=with_labels, node_size=20, node_shape='8')
 
@@ -69,7 +41,7 @@ def pairs_this_round(am, active_nodes, executed_pairs):
     
     for r in range(len(am)):
         for c in range(len(am[0])):
-            if (r,c) not in executed_pairs:
+            if (r,c) not in executed_pairs and (c,r) not in executed_pairs:
                 if am[r][c] and (active_nodes[r] or active_nodes[c]):
                     if r not in participants and c not in participants:
                         pairs.append((r, c))
@@ -116,14 +88,14 @@ def watts_strogatz(N=12,k=4,p=0.4):
     pos = to_ring(N)
 
     # Draw the graph using the polar coordinates
-    draw(G, pos) 
+    draw(G, pos)
 
 
 if __name__ == '__main__':
 
     # RANDOM NETWORK
     N_NEIGHBORS = 4
-    N_NODES = 20
+    N_NODES = 100
 
     # PLANTED PARTITION NETWORK (remove)
     N_NODES_PER_COMMUNITY = 4
@@ -133,36 +105,59 @@ if __name__ == '__main__':
     P_REWIRE = 0.2
 
     # GAME
-    N_ROUNDS = 5
+    MAX_ROUNDS = 20
 
     random_G = nx.random_regular_graph(N_NEIGHBORS, N_NODES)
+    ws_G = nx.watts_strogatz_graph(N_NODES,N_NEIGHBORS,P_REWIRE)
     pos = to_ring(N_NODES)
-    draw(random_G, pos)
-
-    watts_strogatz(N_NODES, N_NEIGHBORS, P_REWIRE)
     
+    # draw(random_G, pos)
+    # draw(ws_G, pos)
 
-    # clustered_G = nx.planted_partition_graph(N_COMMUNITY,
-    #                             N_NODES_PER_COMMUNITY,
-    #                             0.8, 
-    #                             0.05, 
-    #                             directed=False)
-    # regularise_network(clustered_G, N_NEIGHBORS)
-
-    ###################################### ACTIVATION TEST ##########################################################
-
-    active_nodes = np.zeros(N_NODES_PER_COMMUNITY * N_COMMUNITY)
-    active_nodes[0] = 1
-    executed_pairs = []
-
-    am_random = nx.adjacency_matrix(random_G).toarray()
-    for _ in range(N_ROUNDS):
-        pairs, participants = pairs_this_round(am_random, active_nodes, executed_pairs)
-        print(pairs)
-        active_nodes = activate(active_nodes, participants)
-        print(active_nodes)
-
+    # watts_strogatz(N_NODES, N_NEIGHBORS, P_REWIRE)
+    
+    ###################################### ACTIVATION ##########################################################
 
     
+    
 
+    def schedule_network(G, starting_time=8, tpg=5, seeds=[0,5], path = 'MatchingFigures/figures_app/', filename ='Network-schedule.csv'):
+
+        executed_pairs = []
+        filename = path + filename
+        am = nx.adjacency_matrix(G).toarray()
+        active_nodes = np.zeros(N_NODES)
+
+        for seed in seeds:
+            active_nodes[seed] = 1
+
+        time = datetime.timedelta(hours=starting_time)
+
+        file = open(filename, 'w')
+
+        round_count = 1
+        max_game_per_round = 0
+        for _ in range(MAX_ROUNDS):
+            node_colors = ['red' if active_nodes[node-1] else 'skyblue' for node in G.nodes()]
+            # draw(G, pos, node_color=node_colors)
+
+            pairs, participants = pairs_this_round(am, active_nodes, executed_pairs)
+            print(f'num of games this round: {len(pairs)}')
+            active_nodes = activate(active_nodes, participants)
+            print(active_nodes)
+            if pairs:
+                file.write(f'Round {round_count} starts at {str(time)} \n' )
+                file.write(f'Participant IDs: {participants} \n')
+                file.write(str(pairs) + '\n\n\n')
+
+                time += datetime.timedelta(minutes=tpg)
+                round_count += 1
+        
+        file.close()
+
+        return round_count
+
+round_count = schedule_network(random_G, filename='random_schedule.csv')
+round_count = schedule_network(ws_G, filename='ws_schedule.csv')
+print(round_count)
         
