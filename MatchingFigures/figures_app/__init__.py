1e6f7b2c890c5ae5f22a01b18b4f92cd8234a7a6
João Inácio <j.g.c.inacio@fys.uio.no>
2023-09-12 12:56:07 +0200

    working (?) for when players do not arrive all at the same time

--- MatchingFigures/figures_app/__init__.py
+++ MatchingFigures/figures_app/__init__.py
@@ -1,7 +1,6 @@
 from otree.api import *
 from figures_app._utils import *
 import os
-# from figures_app.network_utils import process_txt
 
 doc = """
 Your app description
@@ -15,35 +14,45 @@ class C(BaseConstants):
     ALL_PARTICIPANTS, ALL_PAIRS = process_txt('figures_app/random4242.txt')
  
     NUM_ROUNDS = len(ALL_PARTICIPANTS)
+    GAMES_PER_ROUND = [len(participants) // 2 for participants in ALL_PARTICIPANTS]
+    GAMES_PLAYED = 0
+    
     PAYMENT_PER_CORRECT = 1
-    TIME_RULES = 1 # min
-    TIME_PER_GAME = 4 # min
-    TIME_RESULTS = 15 # sec
+    TIME_RULES = 0.1 # min
+    TIME_PER_GAME = 0.2 # min
+    TIME_RESULTS = 5 # sec
     
     DIR_IMAGES = "original" # ai or original
     NUM_TOTAL = len([file for file in os.listdir(f"_static/global/{DIR_IMAGES}") if file.endswith(".png")])
-    NUM_FIGURES = 6
-    N_SHUFFLE = 3
+    NUM_FIGURES = 4
+    N_SHUFFLE = 2
 
 
 class Subsession(BaseSubsession):
+    games_played = models.IntegerField(initial=0)
+    
     def _create_group_matrix(self):
         pairs = C.ALL_PAIRS[self.round_number-1]
-        matrix = []
+        matrix = list()
         players = self.get_players()
+
         for pair in pairs:
-            group = [players[player_id] for player_id in pair]
+            group = list()
+
+            for player_id in pair: 
+                group.append(players[player_id])
+                players[player_id].playing = 1 if player_id in C.ALL_PARTICIPANTS[players[player_id].subsession.round_number - 1] else 0
+
             matrix.append(group)
+
         return matrix
 
-
-    def group_by_round(self):
-        
+    def _group_by_round(self):
         # New groupings
         matrix = self._create_group_matrix()
         self.set_group_matrix(matrix)
 
-    def assign_cards(self):
+    def _assign_cards(self):
         for group in self.get_groups():
             cards = get_perm(
                 n_players=C.PLAYERS_PER_GROUP, 
@@ -53,27 +62,28 @@ class Subsession(BaseSubsession):
             )
             
             for i, player in enumerate(group.get_players()):
-                print(player.id_in_subsession)
                 player.card0 = cards[i][0]
                 player.card1 = cards[i][1]
                 player.card2 = cards[i][2]
                 player.card3 = cards[i][3]
-                player.card4 = cards[i][4]
-                player.card5 = cards[i][5]
-
+                # player.card4 = cards[i][4]
+                # player.card5 = cards[i][5]
+                
+    def reset(self):
+        self._group_by_round()
+        self._assign_cards()
+        self.games_played = 0
 
 
 def creating_session(subsession: Subsession):
-    subsession.group_by_round()
-    subsession.assign_cards()
-
+    subsession.reset()
 
 class Group(BaseGroup):
     # The correct results should be placed here
     pass
 
 
-def make_result(fig_id):
+def make_result(fig_id: int):
     return models.IntegerField(
         label=f"My Figure {fig_id} corresponds to Figure number ... on my partner's screen  " +\
         f"//  Min figur {fig_id} tilsvarer figur nummer ... på partnerens skjerm.", 
@@ -86,23 +96,23 @@ class Player(BasePlayer):
     # payoff and round_number are defined in the background, don't redefine it.  
 
     score = models.IntegerField(initial=0)
-    rounds_to_play = models.IntegerField(initial=1)
+    playing = models.IntegerField(initial=0)
     
     result0 = make_result(1)
     result1 = make_result(2)
     result2 = make_result(3)
     result3 = make_result(4)
-    result4 = make_result(5)
-    result5 = make_result(6)
+    # result4 = make_result(5)
+    # result5 = make_result(6)
     
     card0 = models.IntegerField()
     card1 = models.IntegerField()
     card2 = models.IntegerField()
     card3 = models.IntegerField()
-    card4 = models.IntegerField()
-    card5 = models.IntegerField()
+    # card4 = models.IntegerField()
+    # card5 = models.IntegerField()
     
-    def get_figure_names(self, indx):
+    def get_figure_names(self, indx: int):
         return [f'global/{C.DIR_IMAGES}/{i}.png' for i in indx]
     
     def get_results(self):
@@ -110,9 +120,9 @@ class Player(BasePlayer):
                 self.result0, 
                 self.result1, 
                 self.result2, 
-                self.result3, 
-                self.result4, 
-                self.result5
+                self.result3#, 
+                # self.result4, 
+                # self.result5
             ]
     
     def get_cards(self):
@@ -120,9 +130,9 @@ class Player(BasePlayer):
                 self.card0, 
                 self.card1, 
                 self.card2, 
-                self.card3, 
-                self.card4, 
-                self.card5
+                self.card3#, 
+                # self.card4, 
+                # self.card5
             ]
     
 
@@ -133,23 +143,24 @@ class Game(Page):
 
     @staticmethod
     def get_form_fields(player: Player):
-        return ['result0', 'result1', 'result2', 'result3', 'result4', 'result5'] if player.id_in_group == C.MAIN_PLAYER_ID else []
+        # return ['result0', 'result1', 'result2', 'result3', 'result4', 'result5'] if player.id_in_group == C.MAIN_PLAYER_ID else []
+        return ['result0', 'result1', 'result2', 'result3'] if player.id_in_group == C.MAIN_PLAYER_ID else []
 
     @staticmethod
     def vars_for_template(self):
         return {
             'ordered_figures': self.get_figure_names(self.get_cards()),
             'text': "Bellow you have to enter THE LABEL of the figure on " +\
-                    "YOUR PARTNER'S SCREEN that matches the FIGURES ON YOUR SCREEN. Note that your answer should be between 1 and 6." +\
+                    "YOUR PARTNER'S SCREEN that matches the FIGURES ON YOUR SCREEN. Note that your answer should be between 1 and 4." +\
                     "  //  Nedenfor må du skrive inn MERKET på figuren på " +\
-                    "PARTNERENS SKJERM som samsvarer med FIGURENE PÅ DIN SKJERM. Vær oppmerksom på at svaret ditt skal ligge mellom 1 og 6." if self.id_in_group == C.MAIN_PLAYER_ID else "" +\
+                    "PARTNERENS SKJERM som samsvarer med FIGURENE PÅ DIN SKJERM. Vær oppmerksom på at svaret ditt skal ligge mellom 1 og 4." if self.id_in_group == C.MAIN_PLAYER_ID else "" +\
                     "Converse with your partner so he/she can input the correct labels of YOUR FIGURES on HIS/HERS ANSWER FORM." +\
                     "  //  Snakk med partneren din, slik at han/hun kan skrive inn de riktige merkelappene for DINE FIGURER på HANS/HENNES SVARSKJEMA.",
             'time': C.TIME_PER_GAME
         }
 
     @staticmethod
-    def before_next_page(player, timeout_happened):
+    def before_next_page(player: Player, timeout_happened: bool):
         if timeout_happened:
             if player.id_in_group == C.MAIN_PLAYER_ID:
                 players = player.group.get_players()
@@ -162,9 +173,12 @@ class Game(Page):
                 for player_ in players:
                     player_.score = score
 
+                player.subsession.games_played += 1 
+                    
     @staticmethod
     def is_displayed(player: Player):
-        return player.id_in_subsession-1 in C.ALL_PARTICIPANTS[player.subsession.round_number-1]
+        return player.playing
+
 
 class Results(Page):
     timeout_seconds = C.TIME_RESULTS
@@ -178,24 +192,24 @@ class Results(Page):
 
     @staticmethod
     def is_displayed(player: Player):
-        return player.id_in_subsession-1 in C.ALL_PARTICIPANTS[player.subsession.round_number-1]
+        return player.playing
 
 
-class WaitForGame(WaitPage):
-    wait_for_all_groups = True
+# class WaitForGame(WaitPage):
+#     wait_for_all_groups = True
     
-    @staticmethod
-    def is_displayed(player: Player):
-        return C.NUM_ROUNDS != 1 and player.group.round_number == 1
+#     @staticmethod
+#     def is_displayed(player: Player):
+#         return C.NUM_ROUNDS != 1 and player.group.round_number == 1
 
 
 class WaitForRound(WaitPage):
-    wait_for_all_groups = True
+    # wait_for_all_groups = True
     template_name = "figures_app/WaitForRound.html"
     
     @staticmethod
     def is_displayed(player: Player):
-        return player.id_in_subsession-1 not in C.ALL_PARTICIPANTS[player.round_number-1]
+        return not player.playing
      
     @staticmethod
     def vars_for_template(self):
@@ -229,18 +243,16 @@ class WaitForRound(WaitPage):
 
 class WaitForStartGame(WaitPage):
     @staticmethod
-    def after_all_players_arrive(group: Group):
-        pass
+    def is_displayed(player: Player):
+        return player.playing
+
 
 class ShuffleWaitPage(WaitPage):
-    wait_for_all_groups = True
-
     @staticmethod
-    def after_all_players_arrive(subsession):
-        subsession.group_by_round()
-        subsession.assign_cards()
-        
-        
+    def after_all_players_arrive(group: Group):
+        if C.GAMES_PLAYED == C.GAMES_PER_ROUND:
+            group.subsession.reset()
+
 
 class Rules(Page):
     timeout_seconds = C.TIME_RULES * 60
@@ -249,10 +261,5 @@ class Rules(Page):
     def is_displayed(player: Player):
         return player.group.round_number == 1
 
-    @staticmethod
-    def vars_for_template(self):
-        return {
-        } 
 
-
-page_sequence = [WaitForGame, Rules, WaitForStartGame, Game, Results, WaitForRound, ShuffleWaitPage] # EndGame
+page_sequence = [Rules, WaitForStartGame, Game, Results, WaitForRound, ShuffleWaitPage] 
